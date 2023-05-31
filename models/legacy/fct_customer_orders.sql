497a7a6fff7df82c8dbee7086844848bfa5e644a
Biayna Grigoryan <biagrigory@gmail.com>
2023-05-31 10:25:33 +0000

    use fully qualified table names and references

--- models/legacy/fct_customer_orders.sql
+++ models/legacy/fct_customer_orders.sql
@@ -32,23 +32,23 @@ paid_orders as
         orders.user_id as customer_id,
         orders.order_date as order_placed_at,
         orders.status as order_status,
-        p.total_amount_paid,
-        p.payment_finalized_date,
-        c.first_name as customer_first_name,
-        c.last_name as customer_last_name
+        successful_payments.total_amount_paid,
+        successful_payments.payment_finalized_date,
+        customers.first_name as customer_first_name,
+        customers.last_name as customer_last_name
     from orders
-    left join successful_payments p on orders.id = p.order_id
-    left join customers c on orders.user_id = c.id 
+    left join successful_payments on orders.id = successful_payments.order_id
+    left join customers on orders.user_id = customers.id 
     ),
 -- Total number of orders per customer
 customer_orders as 
     (select 
-        c.id as customer_id,
+        customers.id as customer_id,
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
         count(orders.id) as number_of_orders
-    from customers c 
-    left join orders on orders.user_id = c.id 
+    from customers
+    left join orders on orders.user_id = customers.id 
     group by 1
     ),
 
@@ -56,28 +56,28 @@ customer_orders as
 
 final as (
 select
-    p.*,
+    paid_orders.*,
 
     -- Sales transcation sequence
-    row_number() over (order by p.order_id) as transaction_seq,
+    row_number() over (order by paid_orders.order_id) as transaction_seq,
 
     -- Customer order/sales sequence
-    row_number() over (partition by customer_id order by p.order_id) as customer_sales_seq,
+    row_number() over (partition by customer_id order by paid_orders.order_id) as customer_sales_seq,
 
     -- New vs returning customer
     case 
-        when c.first_order_date = p.order_placed_at then 'new'
+        when customer_orders.first_order_date = paid_orders.order_placed_at then 'new'
         else 'return' 
     end as nvsr,
 
     -- We need the cumulative CLV, ie growing with each next order and not the total cumulative for each customer id
-    sum(p.total_amount_paid) over (partition by p.customer_id order by p.order_placed_at) as customer_lifetime_value
+    sum(p.total_amount_paid) over (partition by paid_orders.customer_id order by paid_orders.order_placed_at) as customer_lifetime_value
     
     -- First day of sales
-    c.first_order_date as fdos
+    customer_orders.first_order_date as fdos
 
-from paid_orders p 
-left join customer_orders as c using (customer_id)
+from paid_orders
+left join customer_orders using (customer_id)
 
 -- simple select statement
 
